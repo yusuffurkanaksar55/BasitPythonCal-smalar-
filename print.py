5fb8d6b0ca831d32181ac5fb3fe9a39719e91e4b
Yusuf Furkan AKSAR <59505489+yusuffurkanaksar55@users.noreply.github.com>
2020-02-27 01:25:00 +0300

    Create print.py

---
+++ print.py
@@ -0,0 +1,7 @@
+print("Yusuf Furkan Aksar")
+print("İlk deneyim\n")
+print("Birleşik yazım\n")
+print("Yusuf Furkan AKSAR",end="")
+print("İlk deneyim")
+
+print("www","yusuf","aksar",sep="/")#sep="."
