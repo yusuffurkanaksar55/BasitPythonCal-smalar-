b7ecb52004f152000d28a10e2247d851e3f94f8a
Yusuf Furkan AKSAR <59505489+yusuffurkanaksar55@users.noreply.github.com>
2020-02-27 01:24:14 +0300

    Create string.py

---
+++ string.py
@@ -0,0 +1,6 @@
+metin="yusuf furkan aksar"
+print(metin.capitalize())#ilk  harfi büyük yapar
+print(metin.title())# Yusuf Furkan Aksar 
+print(metin.count("u"))#kaç adet u harfi oldugunu gösterir
+print(metin.upper())#büyük harf yapar hepsini
+print(metin.lower())#küçük harf yapar hepsini
