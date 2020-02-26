97438bdbc360bf8f3a639cbd9ecbcf889dea3fd0
Yusuf Furkan AKSAR <59505489+yusuffurkanaksar55@users.noreply.github.com>
2020-02-27 01:25:41 +0300

    Create input.py

---
+++ input.py
@@ -0,0 +1,13 @@
+isim=input("İsminizi giriniz->")
+print(isim)
+
+sayı1=float(input("İlk sayıyı giriniz->"))
+sayı2=float(input("İlk sayıyı giriniz->"))
+print(sayı1)
+print(sayı2)
+topla=sayı1+sayı2
+print(topla)
+print(type(topla))
+
+dönüşüm=int(topla)#float cinsi verilen sayıyı inte cevirdik.
+print(type(dönüşüm))
