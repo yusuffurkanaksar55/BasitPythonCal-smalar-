88be4e0c1100ec76a627f693a7c347c8043e8afd
Yusuf Furkan AKSAR <59505489+yusuffurkanaksar55@users.noreply.github.com>
2020-02-27 01:27:25 +0300

    Create dosyaislemleri.py

---
+++ dosyaislemleri.py
@@ -0,0 +1,6 @@
+dosya=open("dosya.txt","w") #w fonksiyonu dosya olusturup içine yazmayı sağlar. a fonksiyonu var olan dosyanın içine bir şeyler yazdırır
+
+dosya.write("furkan ") 
+
+dosya2=open("dosya2.txt","a")
+dosya2.write("yusuf")
